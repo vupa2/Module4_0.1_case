a9dbf9f2976402992b8de465d368d051463c2f06
vupa2 <anhvu.pa96@gmail>
2021-06-30 21:29:04 +0700

    First commit

---
+++ laravel/app/Http/Controllers/Frontend/ProvinceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Province;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProvinceController extends Controller
+{
+    public function getJobProvinces()
+    {
+        $jobProvince = Province::all();
+        return response()->json($jobProvince);
+    }
+
+    public function index() {
+        $provinces = Province::all();
+        return response()->json($provinces);
+    }
+}
